dcb0bf3ae891dae768ce89512b56d137176a52b3
dennishasnoaccount@googlemail.com <dennishasnoaccount@googlemail.com@15d68e8e-d3f0-6d76-aef1-eb38da93a7c3>
2013-03-16 22:08:22 +0000

    - updated run-script for linux

--- trunk/qtetramesher.sh
+++ trunk/qtetramesher.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# You probably want to edit the following line to match the path to you SOFA instance. Don't forget to append the 'lib' folder.
 SOFA_LIBS_PATH=/opt/sofa/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SOFA_LIBS_PATH:CGALTetrahedralize/lib:SofaTetraStuffing/lib:TetraMeshTools/lib
 ./QTetraMesher
