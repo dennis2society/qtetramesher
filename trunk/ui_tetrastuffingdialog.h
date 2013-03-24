81d991bc47b2ec390a8c1101dc23f603ba8e9c91
dennishasnoaccount@googlemail.com <dennishasnoaccount@googlemail.com@15d68e8e-d3f0-6d76-aef1-eb38da93a7c3>
2013-03-24 13:25:50 +0000

    - updated help-message in menu - added application version string to .pro file and UI help string - fixed SofaTetraStuffing by updating SOFA libs and DLLs - copy own DLLs to Win32 deployment directory automatically when building a new version

--- trunk/ui_tetrastuffingdialog.h
+++ trunk/ui_tetrastuffingdialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'tetrastuffingdialog.ui'
 **
-** Created: Sun 17. Mar 22:22:31 2013
+** Created: Sun 24. Mar 14:02:17 2013
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
