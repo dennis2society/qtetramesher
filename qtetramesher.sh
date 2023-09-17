6734d82d479bbca124107d52f183532b2024ae89
Dennis Luebke <github@dennis2society.de>
2023-09-17 14:42:16 +0200

    - removed obsolete output directories - updated linux run script

--- qtetramesher.sh
+++ qtetramesher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # You probably want to edit the following line to match the path to you SOFA instance. Don't forget to append the 'lib' folder.
-SOFA_LIBS_PATH=/opt/sofa/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:$SOFA_LIBS_PATH:CGALTetrahedralize/lib:SofaTetraStuffing/lib:TetraMeshTools/lib::quartet_tetrastuffing/lib
-./bin_linux_x64/QTetraMesher
+SOFA_LIBS_PATH=/opt/sofa_git/build/install/lib
+cd bin_linux_x64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:$SOFA_LIBS_PATH::.
+./QTetraMesher
