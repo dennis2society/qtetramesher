51791b429dfba3c481f20ccd4280a796fa77db02
Dennis Luebke <github@dennis2society.de>
2023-09-18 19:13:16 +0200

    - finally fixed the CMake custom_commands in Ubuntu   (Ubuntu is not LINUX in CMake but UNIX...) - prepared the linux run script for later

--- qtetramesher.sh
+++ qtetramesher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# You probably want to edit the following line to match the path to you SOFA instance. Don't forget to append the 'lib' folder.
+# Update this line to match your current Sofa installation!
 SOFA_LIBS_PATH=/opt/sofa_git/build/install/lib
-cd bin_linux_x64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:$SOFA_LIBS_PATH::.
+cd build
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:$SOFA_LIBS_PATH:.
 ./QTetraMesher
