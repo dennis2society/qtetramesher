8c9a72714afb01a909ed85aa0d7943e223625968
Dennis Luebke <github@dennis2society.de>
2023-09-22 11:15:33 +0200

    - more code beautificattion

--- TetraMeshTools/include/TetraMeshTools/TetgenLoader.h
+++ TetraMeshTools/include/TetraMeshTools/TetgenLoader.h
@@ -5,48 +5,33 @@
  *      Author: Dennis Luebke
  */
 
-#ifndef TETGENLOADER_H_
-#define TETGENLOADER_H_
+#pragma once
 
 #include "BaseMeshLoader.h"
-
 #include "TetraToolsExports.h"
 
 namespace TetraTools
 {
+  class DLL_EXPORT TetgenLoader : public BaseMeshLoader
+  {
+  protected:
+    std::string _fileName;
+    std::vector<Vec3f>      _vertices;
+    std::vector<Tetrahedron>  _tetraIndices;
+    virtual bool ParseFile(std::ifstream& file_);
+    void ParseTetgen(std::ifstream &file_);
+    void ParseNodes();
+    void ParseElems();
 
-	class DLL_EXPORT TetgenLoader : public BaseMeshLoader
-	{
-	protected:
-		std::string _fileName;
-
-		std::vector<Vec3f>			_vertices;
-		std::vector<Tetrahedron>	_tetraIndices;
-
-		virtual bool ParseFile(std::ifstream& file_);
-
-		void ParseTetgen(std::ifstream &file_);
-
-		void ParseNodes();
-
-		void ParseElems();
-
-	public:
-		TetgenLoader();
-
-		TetgenLoader(const std::string& fileName_);
-
-		bool Load(const std::string& fileName_);
-
-		const std::vector<Vec3f>& GetVertices() const;
-
-		const std::vector<Tetrahedron>& GetTetras() const;
-
-		bool IsLoaded();
-
-		void Clear();
-	};
-
+  public:
+    TetgenLoader();
+    TetgenLoader(const std::string& fileName_);
+    bool Load(const std::string& fileName_);
+    const std::vector<Vec3f>& GetVertices() const;
+    const std::vector<Tetrahedron>& GetTetras() const;
+    bool IsLoaded();
+    void Clear();
+  };
 } /// end namespace Mesh
 
-#endif /* GMSHMESHLOADER_H_ */
+
